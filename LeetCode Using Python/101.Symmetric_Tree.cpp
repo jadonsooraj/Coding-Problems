757eac2652dddaaf6dd8610b2e751f7d7a6724f6
jadonsooraj <jadonsuraj49@gmail.com>
2024-06-21 10:55:36 +0530

    Python problem add

---
+++ LeetCode Using Python/101.Symmetric_Tree.cpp
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSymmetric(self, root):
+        def dfs(root1, root2):
+            if root1 is None and root2 is None:
+                return True
+            if root1 is None or root2 is None or root1.val != root2.val:
+                return False
+            return dfs(root1.left, root2.right) and dfs(root1.right, root2.left)
+
+        return dfs(root, root)
+        
