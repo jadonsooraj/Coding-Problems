935115e96e37ca8c347766e11c1490b9b5d9c6d9
jadonsooraj <jadonsuraj49@gmail.com>
2024-07-14 22:01:04 +0530

    problem add

--- GFG/Sort_a_String.cpp
+++ GFG/Sort_a_String.cpp
@@ -1,12 +1,12 @@
 string sort(string s){
-    //complete the function here
+    //store frequency of chars
     vector<int> hash(26,0);
     string ans;
     
     for(int i=0;i<s.size();i++){
         hash[s[i]-'a']++;
     }
-    
+    //puush_back chars from hash as it is sorted aphabet
     for(int i=0;i<26;i++){
         while(hash[i]>0){
             ans.push_back('a'+i);
